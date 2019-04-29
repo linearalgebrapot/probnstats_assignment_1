7157198e459af921709bc2b997f3d88b01c5fc48
2018008459 <2018008459@hanyang.ac.kr>
2019-04-29 22:36:01 +0900

    Poisson Dist

---
+++ prob_assignment_1/src/prob_assignment_1/Prob.java
@@ -0,0 +1,15 @@
+package prob_assignment_1;
+
+public class Prob {
+
+	public static void main(String[] args) {
+		PoissonDist poissonDist = new PoissonDist(0.05, 1000, 100, 1000000);
+		double[] poissonDistArray = poissonDist.run();
+		for(int i=0; i<poissonDistArray.length; i++) {
+			System.out.println(i + " " + poissonDistArray[i]);
+		}
+		System.out.println();
+		System.out.println("lamda: " + poissonDist.lamda());
+	}
+
+}
